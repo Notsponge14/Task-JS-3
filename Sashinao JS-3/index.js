32c1ce83cfe2cf33f81d1146b6bebd5ad444f3da
Notsponge <arevadzezu2009@gmail.com>
2024-12-14 18:25:32 +0200

    Add files via upload

---
+++ Sashinao JS-3/index.js
@@ -0,0 +1,50 @@
+//Task 1
+
+
+// let score = 85;
+
+
+
+// switch (true) {
+
+//   case (score === 100):
+//   console.log("A+")
+//   break;
+
+//   case (score => 90 || score < 100):
+//   console.log("A")
+//   break;
+
+//   case (score <= 90 || score > 80):
+//   console.log("B")
+//   break;
+
+//   case (score <= 80 || score > 70):
+//   console.log("C")
+//   break;
+
+//   case (score <= 70 || score > 60):
+//   console.log("F")
+//   break;
+
+//   case (score < 60 ):
+//   console.log("You failed the test")
+//   break;
+
+// }
+
+
+
+
+//Task 2
+
+// let x = 9.656;
+// let result = x.toFixed(2)
+// console.log(result)
+
+
+// let x = 102;
+// let result = Number(x);
+
+// console.log(result)
+// console.log(typeof result)
